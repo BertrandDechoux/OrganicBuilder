7d7ac01447738efd020bc44a09161134c0b2995d
dechouxb <dechouxb@e5498eaf-f223-0410-a9d1-935afe5f8a4f>
2007-03-21 06:28:38 +0000

    The source-files are now fully compliant with the use of GPL.
    For information
    "Whichever license you plan to use, the process involves adding two 
    elements to each source file of your program: a copyright notice (such 
    as “Copyright 1999 Terry Jones”), and a statement of copying permission, 
    saying that the program is distributed under the terms of the GNU 
    General Public License (or the Lesser GPL)."
    
    Theses two elements have been added for each file.
    
    
    
    git-svn-id: https://organicbuilder.svn.sourceforge.net/svnroot/organicbuilder/trunk@30 e5498eaf-f223-0410-a9d1-935afe5f8a4f

--- src/uk/org/squirm3/engine/IEngineListener.java
+++ src/uk/org/squirm3/engine/IEngineListener.java
@@ -1,5 +1,27 @@
 package uk.org.squirm3.engine;
 
+
+/**  
+Copyright 2007 Tim J. Hutton, Ralph Hartley, Bertrand Dechoux
+
+This file is part of Organic Builder.
+
+Organic Builder is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+Organic Builder is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Foobar; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
 public interface IEngineListener {
 
 	public void atomsHaveChanged();
--- src/uk/org/squirm3/ui/CurrentLevelListener.java
+++ src/uk/org/squirm3/ui/CurrentLevelListener.java
@@ -21,6 +21,27 @@ import uk.org.squirm3.data.Level;
 import uk.org.squirm3.engine.EngineListenerAdapter;
 import uk.org.squirm3.engine.IApplicationEngine;
 
+
+/**  
+Copyright 2007 Tim J. Hutton, Ralph Hartley, Bertrand Dechoux
+
+This file is part of Organic Builder.
+
+Organic Builder is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+Organic Builder is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Foobar; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
 public class CurrentLevelListener extends EngineListenerAdapter {
 	private IApplicationEngine iApplicationEngine;
 	private JEditorPane description;
